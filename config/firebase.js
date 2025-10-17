1f8466e0c04e326d9549ea02860dc7cb45bd637a
Timeo Santos <tim@MacBook-Air-de-Timeo.local>
2025-10-17 23:48:15 +0200

    Correction des variables d'environnement pour Vercel

--- config/firebase.js
+++ config/firebase.js
@@ -1,11 +1,13 @@
 // config/firebase.js
 
+// 1. Les imports doivent toujours être en premier.
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
-// Le code va maintenant chercher les noms de variables SIMPLES, que Vercel comprend.
+// 2. Le code va maintenant chercher les noms de variables SIMPLES, que Vercel comprend.
+// Il ne doit plus y avoir de "EXPO_PUBLIC_" ici.
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
@@ -15,8 +17,10 @@ const firebaseConfig = {
   appId: process.env.FIREBASE_APP_ID,
 };
 
+// 3. Enfin, on initialise Firebase et on exporte les services.
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+
